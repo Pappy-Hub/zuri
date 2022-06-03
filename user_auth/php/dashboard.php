3b1b11fe0aac6c7f1a9015d0e0f350690f846571
Pappy-Hub <boyshokan@gmail.com>
2022-06-03 23:38:36 +0100

    work in progress

--- user_auth/php/dashboard.php
+++ user_auth/php/dashboard.php
@@ -1,10 +1,10 @@
-<?php 
+<?php
 session_start();
 
 if (!isset($_SESSION["email"]))
     header("Location: ../forms/login.html");
-    //exit;
+//exit;
 
-else  
-session_unset();
-    echo $_SESSION["email"]." WELCOME TO ZURI TRAINING ". ' <a href="../php/logout.php"> <b> LOGOUT <b> </a>';
+else
+    session_unset();
+echo $_SESSION["email"] . " WELCOME TO ZURI TRAINING " . ' <a href="../php/logout.php"> <b> LOGOUT <b> </a>';
