aafcaa1c951c2eab248fb518c8303f63aad30ef7
Pappy-Hub <boyshokan@gmail.com>
2022-06-14 01:58:25 +0100

    unknown errors | replaced deleted html files | new task

--- registration_form/user_data.php
+++ registration_form/user_data.php
@@ -1,21 +1,70 @@
 <?php
 
-$name = $_POST['name']; 
-$email = $_POST['email'];
-$dob = $_POST['dob'];
-$gender = $_POST['gender'];
-$country = $_POST['country'];
+//METHOD ONE
 
-$data = [ $name .", ". $email .", ". $dob .", ". $gender .", ". $country ."\n"]; 
+extract($_REQUEST);
+$filename = fopen("userdata.csv", "a"); //open the filename in a read mode
 
-foreach($data as $value){
-    print_r("$value", "\n");
-}
+fwrite($filename, "Name :");
+fwrite($filename, $name . "\n");
+fwrite($filename, "Email :");
+fwrite($filename, $email . "\n");
+fwrite($filename, "Date of birth :");
+fwrite($filename, $dob . "\n");
+fwrite($filename, "Gender :");
+fwrite($filename, $gender . "\n");
+fwrite($filename, "Country :");
+fwrite($filename, $country . "\n");
 
-$file = 'userdata.csv';
-$handle = fopen("userdata.csv", "a");
-fputcsv($handle, $data);
-fclose($handle);
+fclose($filename); //close filename
 
 
-?>
+//METHOD TWO
+
+// $name = $_POST['name'];
+// $email = $_POST['email'];
+// $dob = $_POST['dob'];
+// $gender = $_POST['gender'];
+// $country = $_POST['country'];
+
+// $data = [$name . ", " . $email . ", " . $dob . ", " . $gender . ", " . $country . "\n"];
+
+// foreach ($data as $value) {
+//     print_r("$value", "<br>");
+// }
+
+// $file = 'userdata.csv';
+// $handle = fopen("userdata.csv", "a");
+// fputcsv($handle, $data);
+// fclose($handle);
+
+
+//METHOD THREE NOT YET WORKING PROPERLY 
+
+// $name = $_POST['name'];
+// $email = $_POST['email'];
+// $dob = $_POST['dob'];
+// $gender = $_POST['gender'];
+// $country = $_POST['country'];
+
+// $handle = fopen("userdata.csv", "a");
+
+// $inputs = "NAME EMAIL DATE OF BIRTH, GENDER, COUNTRY" . "\r";
+// $addcontent = $name . ", " . $email . ", " . $dob . ", " . $gender . ", " . $country . "\n";
+// $filecontents = " ";
+
+// if (filesize("userdata.csv") > 0) {
+//     $handle = fopen("userdata.csv", "r");
+//     $filecontents = fread($handle, filesize("userdata.csv"));
+// }
+
+// $handle = fopen("userdsata.csv", "a");
+
+// if (filesize("userdata.csv") == 0)
+//     fwrite($handle, $inputs);
+
+// fwrite($handle, $inputs);
+
+// fclose($handle);
+
+// print_r($filecontents);
