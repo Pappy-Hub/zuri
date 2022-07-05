a2c7c2ac1123f2e2fb8ba1279f2d71bfb6d8b3ac
Pappy-Hub <boyshokan@gmail.com>
2022-07-06 00:30:37 +0100

    hello laravel

---
+++ zuriLaravel/resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    echo "<h1> Hello Laravel </h1>"
+    ?>
+    <h2>Hello Laravel</h2>
+    <h3>Hello Laravel</h3>
+    <h4>Hello Laravel</h4>
+    <h5>Hello Laravel</h5>
+</body>
+
+</html>
